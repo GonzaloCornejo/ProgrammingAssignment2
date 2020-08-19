78a621939740abf696f3be30cfea20e47916998b
Gonzalo Cornejo <gonza_cornejo@icloud.com>
2020-08-18 19:21:30 -0500

    Update cachematrix.R
    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,17 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
+# This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) { # define argument with default "matrix
+  inv <- NULL # initialize inv as NULL, holds inverse matrix
+  set <- function(y){ #define the new function
+    x <<- y # stores the value in the environment
+    inv <<- NULL  # if there is a new matrix, reset inv to NULL
   }
-  get <- function() {x}
-  setInverse <- function(inverse) {inv <<- inverse}
-  getInverse <- function() {inv}
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  get <- function() {x} # define the get fucntion
+  setInverse <- function(inverse) {inv <<- inverse} # assigns value of inv in parent environment
+  getInverse <- function() {inv} # gets the value of inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) # you need this in order to use the $
 }
 
 
